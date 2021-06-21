33ae50a9d18e586641f8f16c3a0f2c4851501d7b
leimingting <1042808424@qq.com>
2021-06-21 14:09:21 +0800

    tow commit

---
+++ src/main/java/com/leimingting/utils/Main.java
@@ -0,0 +1,11 @@
+package com.leimingting.utils;
+
+/**
+ * @Author 小T
+ * @Date 2021/6/21 14:08
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
